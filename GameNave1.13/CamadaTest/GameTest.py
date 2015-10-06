3882ce4963a866eaec7b065ca37d67a8bcc534b7
Izabely Furtado <izabelyfurtado1@gmail.com>
2015-10-06 07:46:40 -0300

    Update GameTest.py

--- GameNave1.13/CamadaTest/GameTest.py
+++ GameNave1.13/CamadaTest/GameTest.py
@@ -6,7 +6,7 @@ import unittest
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)
+        self.assertEqual(True, True)
 
     def cria_posicao_valido(self):
         val = 100
